e82ecfc6817a5b2684f828a1d6277503e160f6af
Anthony Chambers <4009987+chambersaj@users.noreply.github.com>
2024-01-30 13:36:33 -0500

    Update s3.tf

--- code/build/s3.tf
+++ code/build/s3.tf
@@ -20,3 +20,11 @@ resource "aws_s3_bucket" "dev_s3" {
     user                 = "achambers"
   }
 }
+
+resource "aws_s3_bucket_ownership_controls" "dev_s3" {
+  bucket = aws_s3_bucket.dev_s3.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
